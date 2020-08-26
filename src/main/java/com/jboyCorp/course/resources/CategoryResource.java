a0169d451aa34d478106a15bffc094ee5b3a8cf4
joaoalflopes <68ronins@gmail.com>
2020-08-26 04:39:21 -0300

    Implementing put method for entity Category

--- src/main/java/com/jboyCorp/course/resources/CategoryResource.java
+++ src/main/java/com/jboyCorp/course/resources/CategoryResource.java
@@ -42,4 +42,11 @@ public class CategoryResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Long id ){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/jboyCorp/course/services/CategoryService.java
+++ src/main/java/com/jboyCorp/course/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jboyCorp.course.entities.Category;
 import com.jboyCorp.course.repositories.CategoryRepository;
+import com.jboyCorp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -19,13 +20,22 @@ public class CategoryService {
 		return repository.findAll();
 	}
 	
-	public Category findById(Long id) {
+	public Category findById(Long id){
 		Optional<Category> obj = repository.findById(id);
+		if(obj == null) {
+			throw new ResourceNotFoundException("Object Not Found! id: " + id
+					+ ", Type: " + Category.class.getName());	
+		}
 		return obj.get();
 	}
 	
 	public Category insert(Category obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public Category update(Category obj) {
+		findById(obj.getId());
 		return repository.save(obj);
 	}
 }
