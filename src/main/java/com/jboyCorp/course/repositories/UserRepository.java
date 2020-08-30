e0a1f9e025c7b8eeb4100caeb4f5d4d63b1904b1
joaoalflopes <68ronins@gmail.com>
2020-08-30 18:49:44 -0300

    Customized email validation: not duplicated in insert method for user entity

--- src/main/java/com/jboyCorp/course/repositories/UserRepository.java
+++ src/main/java/com/jboyCorp/course/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.jboyCorp.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jboyCorp.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Transactional(readOnly = true)
+	User findByEmail(String email);
 
 }
--- src/main/java/com/jboyCorp/course/services/validation/UserInsertValidator.java
+++ src/main/java/com/jboyCorp/course/services/validation/UserInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.jboyCorp.course.dto.UserNewDTO;
+import com.jboyCorp.course.entities.User;
 import com.jboyCorp.course.entities.enums.TypeClient;
+import com.jboyCorp.course.repositories.UserRepository;
 import com.jboyCorp.course.resources.exceptions.FieldMessage;
 import com.jboyCorp.course.services.validation.utils.BR;
 
 public class UserInsertValidator implements ConstraintValidator<UserInsert, UserNewDTO> {
+	
+	@Autowired
+	private UserRepository repository;
+	
 	@Override
 	public void initialize(UserInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class UserInsertValidator implements ConstraintValidator<UserInsert, User
 		
 		if(objDTO.getTypeClient().equals(TypeClient.PESSOAJURIDICA.getCode()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "Invalid CNPJ."));
+		}
+		
+		User aux = repository.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email is registered in the database"));
 		}
 		
 		
