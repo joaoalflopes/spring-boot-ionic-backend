400b8302e197f0e8afc1c2a20ef54ed417c83463
joaoalflopes <68ronins@gmail.com>
2020-09-29 22:15:08 -0300

    Saving first file to Bucket S3

--- src/main/java/com/jboyCorp/course/CourseApplication.java
+++ src/main/java/com/jboyCorp/course/CourseApplication.java
@@ -1,11 +1,18 @@
 package com.jboyCorp.course;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jboyCorp.course.services.S3Service;
+
 @SpringBootApplication
 public class CourseApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
@@ -13,8 +20,7 @@ public class CourseApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+		s3Service.uploadFile("C:\\Users\\j.lopes\\Downloads\\testAWS\\Penguins.jpg");
 	}
 
 }
