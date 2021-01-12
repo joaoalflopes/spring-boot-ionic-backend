7df4d9891a883468da1335849753fb9ac192c967
joaoalflopes <68ronins@gmail.com>
2021-01-12 19:12:54 -0300

    Changing Cors configuration to accept put and delete

--- src/main/java/com/jboyCorp/course/config/SecurityConfig.java
+++ src/main/java/com/jboyCorp/course/config/SecurityConfig.java
@@ -77,8 +77,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configCors = new CorsConfiguration().applyPermitDefaultValues();
+		configCors.setAllowedMethods(Arrays.asList("POST","GET","PUT","DELETE","OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configCors);
 		return source;
 	}
 	
